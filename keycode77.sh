b64864ab28182396a3de07b5fcf528119c545f55
Flavio de Barros <76204933+Nevyle@users.noreply.github.com>
2023-02-28 10:36:31 -0300

    keycode77.sh V3
    Fixing the bug that gives the possibility to disable the managerial numlock together with the shift key

--- keycode77.sh
+++ keycode77.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xmodmap -e "keycode 77 = NoSymbol Num_Lock"
+xmodmap -e "keycode 77 = NoSymbol NoSymbol"
 echo "Script de trava do NumLock executado em:" >> ~/.log.txt && date "+(%A) %d de %B de %Y às %T" >> ~/.log.txt && echo "" >> ~/.log.txt
